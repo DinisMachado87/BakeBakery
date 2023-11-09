ebb43b3d56d3e4b303d4d8e412c9f139150c18c0
Dinis Machado <137174511+DinisMachado87@users.noreply.github.com>
2023-11-09 00:12:05 +0000

    Create a dictionary with receipe quantities

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+pantry_targets = {
+    flour: 1 kg
+    eggs: 6 unit
+    yeast: 200 gr
+    butter: 0.4 kg
+    chocolate: 200 gr
+    sugar: 400 gr
+    salt: 
+}
+
+pantry = {
+    flour: 1 kg
+    eggs: 6 unit
+    yeast: 200 gr
+    butter: 0.4 kg
+    chocolate: 200 gr
+    sugar: 400 gr
+}
+
+croissant_ = {
+    ingredients = {
+        "flour (g)": 400,
+        "milk (ml)": 200,
+        "sugar (g)": 40,
+        "salt (g)": 8,
+        "butter (g)": 200,
+        "yeast (g)": 10,
+        "chocolate chips (g)": 100
+        }
+    serves = 10
+}
