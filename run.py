5fc659240c2d83cf31ee0b00f2b21d76db9d5050
Dinis Machado <137174511+DinisMachado87@users.noreply.github.com>
2023-11-14 10:15:13 +0000

    print each recipe dictionary pair in a diferent line

--- run.py
+++ run.py
@@ -12,10 +12,12 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('BakeryBake')
 
-def recipe(baked_goods_page, backed_goods):
-    recipe_croisants = SHEET.worksheet(baked_goods_page)
-    data = worksheet.backed_goods_page.get_all_values()
+def recipe(baked_goods_page, baked_goods):
+    worksheet = SHEET.worksheet(baked_goods_page)
+    data = worksheet.get_all_values()
     baked_goods = {column[0]: column[2] for column in data}
-    print(backed_goods)
+    for key, value in baked_goods.items():
+        print(f"{key}: {value}")
+    return baked_goods
 
 recipe('recipe_croisants', 'croisants')
